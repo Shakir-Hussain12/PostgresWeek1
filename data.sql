0f1b2d70eebf6cbf1e7d6b0202d0610f03edb7d7
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-03-30 14:56:15 +0500

    Created table, inserted data, ran queries

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
-/* Populate database with sample data. */
+/* Populate database
+WITH sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES (1, 'Agumon', '02/03/2020', 0, 'T', 10.23);
+INSERT INTO animals VALUES (2, 'Gabumon', '09/15/2018', 2, 'T', 8);
+INSERT INTO animals VALUES (3, 'Pikachu', '01/07/2021', 1, 'F', 15.04);
+INSERT INTO animals VALUES (4, 'Devimon', '05/12/2017', 5, 'T', 11);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,35 @@
-/*Queries that provide answers to the questions from all projects.*/
+/*Queries that provide answers to the questions
+FROM all projects.*/
+SELECT  *
+FROM animals
+WHERE trim(name) like '%mon';
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT  *
+FROM animals
+WHERE extract('year'
+FROM date_of_birth) BETWEEN 2016 AND 2019;
+
+SELECT  *
+FROM animals
+WHERE neutered = true
+AND escape_attempts < 3;
+
+SELECT  date_of_birth
+FROM animals
+WHERE name IN ('Agumon', 'Pikachu');
+
+SELECT  name,escape_attempts
+FROM animals
+WHERE weight_kg > 10.5;
+
+SELECT  *
+FROM animals
+WHERE neutered = true;
+
+SELECT  *
+FROM animals
+WHERE name != 'Gabumon';
+
+SELECT  *
+FROM animals
+WHERE weight_kg BETWEEN 10.4 AND 17.3;
