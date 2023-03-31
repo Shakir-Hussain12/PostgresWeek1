51d33b93844212a8dc36f29aeb19fce1a3f723f1
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-03-31 10:18:20 +0500

    Added Species Column,Tested Aggregate And Transactional Queries

--- data.sql
+++ data.sql
@@ -1,7 +1,15 @@
 /* Populate database
 WITH sample data. */
-
 INSERT INTO animals VALUES (1, 'Agumon', '02/03/2020', 0, 'T', 10.23);
 INSERT INTO animals VALUES (2, 'Gabumon', '09/15/2018', 2, 'T', 8);
 INSERT INTO animals VALUES (3, 'Pikachu', '01/07/2021', 1, 'F', 15.04);
 INSERT INTO animals VALUES (4, 'Devimon', '05/12/2017', 5, 'T', 11);
+
+ALTER TABLE animals add column species char(30);
+INSERT INTO animals VALUES (5, 'Charmandar', '02/08/2020', 0, 'F', -11);
+INSERT INTO animals VALUES (6, 'Plantom', '11/15/2021', 2, 'T', -5.7);
+INSERT INTO animals VALUES (7, 'Squirtle', '04/02/1993', 3, 'F', -12.13);
+INSERT INTO animals VALUES (8, 'Angemon', '06/12/2005', 1, 'T', -45);
+INSERT INTO animals VALUES (9, 'Boarmon', '06/7/2020', 7, 'T', 20.4);
+INSERT INTO animals VALUES (10, 'Blossom', '10/13/1998', 3, 'T', 17);
+INSERT INTO animals VALUES (11, 'Ditto', '05/14/2022', 4, 'F', 22);
--- queries.sql
+++ queries.sql
@@ -32,4 +32,51 @@ WHERE name != 'Gabumon';
 
 SELECT  *
 FROM animals
-WHERE weight_kg BETWEEN 10.4 AND 17.3;
+WHERE weight_kg BETWEEN 10.4 AND 17.3; begin; update animals
+
+SET species = 'nonspecified';
+
+SELECT  *
+FROM animals; rollback; begin; update animals
+
+SET species = 'digimon'
+WHERE trim(name) like '%mon'; update animals
+
+SET species = 'pokemon'
+WHERE species is NULL; commit; begin; delete
+FROM animals;
+
+SELECT  *
+FROM animals; rollback;
+
+SELECT  *
+FROM animals; begin; delete
+FROM animals
+WHERE date_of_birth > '01/01/2022'; savepoint neg; update animals
+
+SET weight_kg = (weight_kg * -1); rollback to neg; update animals
+SET weight_kg = (weight_kg * -1)
+WHERE weight_kg < 0; release savepoint neg; commit;
+
+SELECT  COUNT(*)
+FROM animals;
+
+SELECT  COUNT(*)
+FROM animals
+WHERE escape_attempts = 0;
+
+SELECT  AVG(weight_kg)
+FROM animals
+
+SELECT  neutered,SUM(escape_attempts) AS total_escape_attempts
+FROM animals
+GROUP BY  neutered
+
+SELECT  species, MAX(weight_kg) AS max_weight, MIN(weight_kg) AS min_weight
+FROM animals
+GROUP BY  species
+SELECT  species,AVG(escape_attempts) AS avg_escapes
+FROM animals
+WHERE extract('year'
+FROM date_of_birth) BETWEEN 1990 AND 2000
+GROUP BY  species
